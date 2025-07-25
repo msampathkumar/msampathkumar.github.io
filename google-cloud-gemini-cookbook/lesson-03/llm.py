fd7620c31454185aae516ca797480be910b94c1f
Sampath Kumar <sam1990kumar@gmail.com>
2025-07-25 11:01:38 +0200

    Deployed 94cb49c with MkDocs version: 1.6.1

--- google-cloud-gemini-cookbook/lesson-03/llm.py
+++ google-cloud-gemini-cookbook/lesson-03/llm.py
@@ -2,27 +2,29 @@ from google import genai
 from google.genai.types import HttpOptions, GenerateContentConfig
 
 # Gemini AI model name
-# MODEL_NAME = "gemini-2.0-flash-lite-001"
-MODEL_NAME = "gemini-2.5-flash-lite-preview-06-17"
+MODEL_NAME = "gemini-2.5-flash"
 
 # Gemini AI client
 #  - Using environment variables to pass essential parameters to the client.
 #  - Read more at https://github.com/googleapis/python-genai/tree/main?tab=readme-ov-file#create-a-client
 client = genai.Client(http_options=HttpOptions(api_version="v1"))
 
+system_instruction = [
+    "You're a helpful Gemini AI Chatbot.",
+    "Answer user's questions and use simple and clear language."
+    "When possible, reply to user's question with a single sentence or a few sentences.",
+    "Free to use emojis."
+    "Be open and friendly. Don't be afraid to ask questions or clarify things.",
+]
+
 
 # Create chat session
-def get_chat_session():
+def get_chat_session(cache_name=None):
     chat_session = client.chats.create(
         model=MODEL_NAME,
         config=GenerateContentConfig(
-            system_instruction=[
-                "You're a helpful Gemini AI Chatbot.",
-                "Answer user's questions and use simple and clear language."
-                "When possible, reply to user's question with a single sentence or a few sentences.",
-                "Free to use emojis."
-                "Be open and friendly. Don't be afraid to ask questions or clarify things.",
-            ]
+            cached_content=cache_name,
+            system_instruction=None if cache_name else system_instruction,
         ),
     )
     # Print chat session ID
